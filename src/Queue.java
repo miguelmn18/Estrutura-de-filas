7ead9e7227ac5ce4938a54c03f51fc1c6624835b
miguelmn18 <miguel.macedo@dcx.ufpb.br>
2025-03-14 14:33:08 -0300

    repositorio java

---
+++ src/Queue.java
@@ -0,0 +1,107 @@
+import org.w3c.dom.Node;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Queue <Item> implements  Iterable <Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
+
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+    }
+
+    public Queue() {
+        first = null;
+        last = null;
+        n = 0;
+    }
+
+    public boolean isEmpty() {
+        // o metodo boolean só retorna true ou false
+        return first = null;
+
+    }
+
+    public int size() {
+        return n;
+    }
+
+    public Item peek() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return first.item;
+
+    }
+
+    public void enqueue(Item item) {
+        Node<Item> oldLast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) first = last;
+        else oldLast.next = last;
+        n++;
+    }
+
+    public Item dequeue() {
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = first.item;
+        first = first.next;
+        n--;
+        if (isEmpty()) last = null;
+        return item;
+    }
+
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        for (Item item : this) {
+            s.append(item);
+            s.append(' ');
+
+        }
+        return s.toString();
+    }
+
+    public Iterator<Item> iterator() {
+        return new LinkedIterator(first);
+    }
+
+    private Node<Item> current;
+
+    private class LinkedIterator implements Iterator<Item> {
+        private Queue.Node<Item> current;
+
+        public LinkedIterator(Queue.Node<Item> first) {
+            current = first;
+        }
+
+        public boolean hasNext() {
+            return current != null;
+
+        }
+
+        public Item next() {
+            if (!hasNext()) throw new NoSuchElementException();
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+    }
+
+    public static void main (String [] args) { // se aqui der erro no StdIn ou StdOut ou não implementou ou classe StdIn e StdOut
+                                                //precisa ser ajustada
+        Queue<String> queue = new Queue<String>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-"))
+                queue.enqueue(item);
+            else if (queue.isEmpty())
+                StdOut.print(queue.dequeue() + " ");
+
+        }
+        StdOut.println ("(" + queue.size() + " left on queue) ");
+    }
+}
---
+++ src/ResizingArrayQueue.java
@@ -0,0 +1,104 @@
+import java.lang.reflect.Array;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class ResizingArrayQueue <Item> implements Iterator <Item> {
+    private static final int INIT_CAPACITY = 8;
+
+    private Item[] q;
+    private int n;
+    private int first;
+    private int last;
+
+    public ResizingArrayQueue() {
+        q = (Item[]) new Object[INIT_CAPACITY];
+        n = 0;
+        first = 0;
+        last = 0;
+    }
+
+    public boolean isEmpty() { // só retorna true ou false;
+        return n == 0;
+    }
+
+    public int size() {
+        return n;
+
+    }
+
+    private void resize(int capacity) {
+        assert capacity >= n;
+        Item[] copy = (Item[]) new Object[capacity];
+        for (int i = 0; i < n; i++) {
+            copy[i] = q[(first + i) % q.length];
+        }
+        q = copy;
+        first = 0;
+        last = n;
+    }
+
+    public void enqueue(Item item) {
+        if (n == q.length) resize(2 * q.length);
+        q[last++] = item;
+        if (last == q.length) last = 0;
+        n++;
+
+    }
+
+    public Item dequeue() { //aqui tem um erro no qual eu não identificar
+        if (isEmpty()) throw new NoSuchElementException("Queue underflow");
+        Item item = q[first];
+        q[first] = null;
+        n--;
+        first++;
+        if(first == q.length) first = 0;
+        if(n > 0  && n = q.length/4) resize(q.length/2);
+        return item;
+    }
+
+    public Item peek (){
+        if(isEmpty()) throw new NoSuchElementException("Queue underflow");
+        return q[first];
+
+    }
+
+    public Iterator<Item> iterator (){
+        return new ArrayIterator();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public Item next() {
+        return null;
+    }
+
+    private class ArrayIterator implements Iterator <Item> {
+        private int i = 0;
+
+        public boolean hasNext() {
+            return i < n;
+        }
+         public Item next(){
+            if(!hasNext ()) throw new NoSuchElementException();
+            Item item = q[(i + first) % q.length];
+            i++;
+            return item;
+         }
+
+    }
+
+    public static void main (String [] args) {
+        ResizingArrayQueue<String> queue = new ResizingArrayQueue<>();
+        while (!StdIn.isEmpty()) {
+            String item = StdIn.readString();
+            if (!item.equals("-")) queue.enqueue(item);
+            else if (!queue.isEmpty()) StdOut.print(queue.dequeue() + " ");
+
+        }
+        StdOut.println ("(" + queue.size() + "left on queue )");
+    }
+}
